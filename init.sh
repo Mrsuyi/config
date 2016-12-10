f2f6f089b73135c3ecc88cad50531cba2c6cd1f1
Mrsuyi <mrsuyi@163.com>
2016-12-10 10:39:48 +0800

    init.sh

--- init.sh
+++ init.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env bash
 
 # vim
-rm -rf ~/.vim && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-rm ~/.vimrc && ln -s ~/config/.vimrc ~
-rm ~/.ycm_extra_conf.py && ln -s ~/config/.ycm_extra_conf.py ~
+rm -rf ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rm -f ~/.vimrc
+ln -s ~/config/.vimrc ~
+rm -f ~/.ycm_extra_conf.py
+ln -s ~/config/.ycm_extra_conf.py ~
 ln -s ~/config/vim/skeleton ~/.vim
 
 # zsh
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-rm ~/.zshrc && ln -s ~/config/.zshrc ~
+sh -c $(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+rm -f ~/.zshrc
+ln -s ~/config/.zshrc ~
 
 # top
-rm ~/.toprc && ln -s ~/config/.toprc ~
+rm -f ~/.toprc
+ln -s ~/config/.toprc ~
 
 # xinit
-rm ~/.xinitrc && ln -s ~/config/.xinitrc ~
+rm -f ~/.xinitrc
+ln -s ~/config/.xinitrc ~
 
 # tmux
-rm ~/.tmux.conf && ln -s ~/config/.tmux.conf ~
-rm ~/.tmux.theme && ln -s ~/config/.tmux.theme ~
+rm -f ~/.tmux.conf
+ln -s ~/config/.tmux.conf ~
+rm -f ~/.tmux.theme
+ln -s ~/config/.tmux.theme ~
 
 # lxterminal
-rm -rf ~/.config/lxterminal && ln -s ~/config/lxterminal ~/.config
+rm -rf ~/.config/lxterminal
+ln -s ~/config/lxterminal ~/.config/
 
 # awesome
-rm -rf ~/.config/awesome && ln -s ~/config/awesome ~/.config
+rm -rf ~/.config/awesome
+ln -s ~/config/awesome ~/.config/
